f1b17aff20d71ae8c583dd2737102c567f98914b
Tony Arcieri <bascule@gmail.com>
2017-02-27 00:47:23 -0800

    Initial implementation
    Uses the `rustsec` crate to fetch the security advisory database, then compares
    it against crates in Cargo.lock.
    
    Provides colorful reports of which crates are vulnerable, then exits with a
    non-zero status if there are any vulnerable crates.
    
    Pretty hacky code... could use a lot of work. But it's a start.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,195 @@
+//! Audit Cargo.lock files for crates containing security vulnerabilities
+
+#![crate_name = "cargo_audit"]
+#![crate_type = "bin"]
+
+#![deny(missing_docs, missing_debug_implementations, missing_copy_implementations)]
+#![deny(trivial_casts, trivial_numeric_casts)]
+#![deny(unsafe_code, unstable_features, unused_import_braces, unused_qualifications)]
+
+extern crate clap;
+extern crate rustsec;
+extern crate semver;
+extern crate term;
+extern crate toml;
+
+use rustsec::AdvisoryDatabase;
+use rustsec::advisory::Advisory;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+use std::process;
+
+const APP_NAME: &'static str = "cargo-audit";
+const VERSION: &'static str = "0.0.0";
+
+// TODO: Use serde
+#[derive(Debug, PartialEq)]
+struct Package {
+    name: String,
+    version: String,
+}
+
+fn load_lockfile(filename: &str) -> Vec<Package> {
+    let path = Path::new(filename);
+    let mut file = File::open(&path).expect("Couldn't open the lockfile!");
+    let mut body = String::new();
+
+    file.read_to_string(&mut body).expect("Error reading lockfile!");
+
+    let toml = body.parse::<toml::Value>().expect("Couldn't parse the lockfile!");
+    let packages = match toml["package"] {
+        toml::Value::Array(ref arr) => arr,
+        _ => panic!("lockfile is malformed!"),
+    };
+
+    packages.iter()
+        .map(|package| {
+            Package {
+                name: String::from(package["name"].as_str().expect("missing package name!")),
+                version: String::from(package["version"]
+                    .as_str()
+                    .expect("missing package version!")),
+            }
+        })
+        .collect()
+}
+
+// TODO: Macros, cleaner API, support for disabling colors (possibly using Cargo settings?)
+// Cargo's `Shell` type may be useful here
+fn notify<T>(terminal: &mut Box<T>, color: term::color::Color, status: &str, message: &str)
+    where T: term::Terminal + ?Sized
+{
+    terminal.attr(term::Attr::Bold).unwrap();
+    terminal.fg(color).unwrap();
+    write!(terminal, "{:>12}", status).unwrap();
+
+    terminal.reset().unwrap();
+    write!(terminal, " {}\n", message).unwrap();
+}
+
+fn display_attribute<T>(terminal: &mut Box<T>, name: &str, value: &str)
+    where T: term::Terminal + ?Sized
+{
+    terminal.attr(term::Attr::Bold).unwrap();
+    terminal.fg(term::color::RED).unwrap();
+    write!(terminal, "{}: ", name).unwrap();
+
+    terminal.reset().unwrap();
+    terminal.attr(term::Attr::Bold).unwrap();
+    write!(terminal, "{}\n", value).unwrap();
+}
+
+fn display_advisory<T>(terminal: &mut Box<T>, package: &Package, advisory: &Advisory)
+    where T: term::Terminal + ?Sized
+{
+    write!(terminal, "\n").unwrap();
+
+    display_attribute(terminal, "ID", &advisory.id);
+    display_attribute(terminal, "Crate", &package.name);
+    display_attribute(terminal, "Version", &package.version);
+
+    if let Some(ref date) = advisory.date {
+        display_attribute(terminal, "Date", date);
+    }
+
+    if let Some(ref url) = advisory.url {
+        display_attribute(terminal, "URL", url);
+    }
+
+    display_attribute(terminal, "Title", &advisory.title);
+
+    let mut fixed_versions = String::new();
+    let version_count = advisory.patched_versions.len();
+
+    for (i, version) in advisory.patched_versions.iter().enumerate() {
+        fixed_versions.push_str(&version.to_string());
+
+        if i < version_count - 1 {
+            fixed_versions.push_str(", ");
+        }
+    }
+
+    display_attribute(terminal, "Solution: upgrade to", &fixed_versions);
+
+    terminal.reset().unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut stdout = term::stdout().unwrap();
+
+    let matches = clap::App::new(APP_NAME)
+        .version(VERSION)
+        .arg_from_usage("-f, --file=[NAME] 'Cargo lockfile to inspect (default: Cargo.lock)'")
+        .arg_from_usage("-u, --url=[URL] 'URL from which to fetch advisory database'")
+        .get_matches();
+
+    let filename = matches.value_of("file").unwrap_or("Cargo.lock");
+    let url = matches.value_of("url").unwrap_or(rustsec::ADVISORY_DB_URL);
+
+    let dependencies = load_lockfile(filename);
+
+    notify(&mut stdout,
+           term::color::GREEN,
+           "Fetching",
+           &format!("advisories `{}`", url));
+
+    let advisory_db = AdvisoryDatabase::fetch_from_url(url)
+        .expect("Couldn't fetch advisory database");
+
+    notify(&mut stdout,
+           term::color::GREEN,
+           "Scanning",
+           &format!("{} crates for vulnerabilities ({} advisories in database)",
+                    dependencies.len(),
+                    advisory_db.iter().len()));
+
+    let mut vuln_count: usize = 0;
+
+    for package in dependencies {
+        let advisories = advisory_db.find_vulns_for_crate(&package.name, &package.version)
+            .expect("error obtaining advisories for this crate");
+
+        if vuln_count == 0 && !advisories.is_empty() {
+            notify(&mut stdout,
+                   term::color::RED,
+                   "Warning",
+                   "Vulnerable crates found!")
+        }
+
+        vuln_count += advisories.len();
+
+        for advisory in advisories {
+            display_advisory(&mut stdout, &package, advisory);
+        }
+    }
+
+    if vuln_count == 0 {
+        notify(&mut stdout,
+               term::color::GREEN,
+               "Success",
+               "No vulnerable packages found");
+
+        process::exit(0);
+    } else {
+        stdout.attr(term::Attr::Bold).unwrap();
+        stdout.fg(term::color::RED).unwrap();
+
+        if vuln_count == 1 {
+            write!(stdout, "\n1 vulnerability found!\n").unwrap();
+        } else {
+            write!(stdout, "\n{} vulnerabilities found!\n", vuln_count).unwrap();
+        }
+
+        stdout.reset().unwrap();
+        process::exit(1);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn load_cargo_lockfile() {
+        ::load_lockfile("Cargo.lock");
+    }
 }
