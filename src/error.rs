68fbc3a459f14168d3ef9b5ff1483e0b5b37e139
Tony Arcieri <bascule@gmail.com>
2017-02-26 18:03:23 -0800

    Make "advisory" and "error" modules public
    They are part of the public API and should be public

--- src/error.rs
+++ src/error.rs
@@ -1,13 +1,27 @@
+//! Error types used by this crate
+
 use std::{fmt, result};
 use std::error::Error as StdError;
 
+/// Custom error type for this library
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub enum Error {
+    /// An error occurred while making a request to the advisory database
     Request,
+
+    /// Advisory database server responded with an error
     Response,
+
+    /// Couldn't parse response data
     Parse,
+
+    /// Response data is missing an expected attribute
     MissingAttribute,
+
+    /// Response data contains an attributed which is the wrong type or otherwise invalid
     InvalidAttribute,
+
+    /// Version requirement is not properly formed
     MalformedVersion,
 }
 
@@ -30,4 +44,5 @@ impl StdError for Error {
     }
 }
 
+/// Custom result type for this library
 pub type Result<T> = result::Result<T, Error>;
