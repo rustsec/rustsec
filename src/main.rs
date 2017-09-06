8e670b0ace821fbf1dfaa1c3b61793ead91268d0
unv Annihilator <unv-annihilator@users.noreply.github.com>
2017-09-06 18:16:09 -0500

    Use crate isatty to resolve Windows build errors
    Error was due to lack of 'isatty()' method and failing to resolve enums in scope.
    Removed semver and toml as they were unused.
    Remove isatty() function infavor of isatty::stdout_isatty()
    Swap cfg(unix) to cfg(not(windows))
    Regenerate Cargo.lock to resolve older Hyper-Tls security vulnerability.
    Cargo format ran over the project after change were made.

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,9 @@
 mod shell;
 
 extern crate clap;
-extern crate libc;
 extern crate rustsec;
-extern crate semver;
 extern crate term;
-extern crate toml;
+extern crate isatty;
 
 use clap::{App, Arg, SubCommand};
 use rustsec::{AdvisoryDatabase, Lockfile};
@@ -27,24 +25,36 @@ use term::color::{RED, GREEN, WHITE};
 
 fn main() {
     let matches = App::new("cargo")
-        .subcommand(SubCommand::with_name("audit")
-            .version(env!("CARGO_PKG_VERSION"))
-            .author("Tony Arcieri <bascule@gmail.com>")
-            .about("Audit Cargo.lock for crates with security vulnerabilities.")
-            .arg_from_usage("-f, --file=[NAME] 'Cargo lockfile to inspect (default: Cargo.lock)'")
-            .arg_from_usage("-u, --url=[URL] 'URL from which to fetch advisory database'")
-            .arg(Arg::from_usage("--color=[COLOR] Colored output")
-                .possible_values(&["auto", "always", "never"])))
+        .subcommand(
+            SubCommand::with_name("audit")
+                .version(env!("CARGO_PKG_VERSION"))
+                .author("Tony Arcieri <bascule@gmail.com>")
+                .about("Audit Cargo.lock for crates with security vulnerabilities.")
+                .arg_from_usage(
+                    "-f, --file=[NAME] 'Cargo lockfile to inspect (default: Cargo.lock)'",
+                )
+                .arg_from_usage(
+                    "-u, --url=[URL] 'URL from which to fetch advisory database'",
+                )
+                .arg(
+                    Arg::from_usage("--color=[COLOR] Colored output")
+                        .possible_values(&["auto", "always", "never"]),
+                ),
+        )
         .get_matches();
 
-    let (filename, url, color_config) = if let Some(audit_matches) =
-        matches.subcommand_matches("audit") {
-        (audit_matches.value_of("file").unwrap_or("Cargo.lock"),
-         audit_matches.value_of("url").unwrap_or(rustsec::ADVISORY_DB_URL),
-         audit_matches.value_of("color").unwrap_or("auto"))
-    } else {
-        panic!("cargo-audit is intended to be invoked as a cargo subcommand");
-    };
+    let (filename, url, color_config) =
+        if let Some(audit_matches) = matches.subcommand_matches("audit") {
+            (
+                audit_matches.value_of("file").unwrap_or("Cargo.lock"),
+                audit_matches.value_of("url").unwrap_or(
+                    rustsec::ADVISORY_DB_URL,
+                ),
+                audit_matches.value_of("color").unwrap_or("auto"),
+            )
+        } else {
+            panic!("cargo-audit is intended to be invoked as a cargo subcommand");
+        };
 
     let mut shell = shell::create(match color_config {
         "always" => ColorConfig::Always,
@@ -61,26 +71,35 @@ fn main() {
         Err(ex) => panic!("Couldn't load {}: {}", filename, ex),
     };
 
-    shell.say_status("Fetching", &format!("advisories `{}`", url), GREEN, true).unwrap();
+    shell
+        .say_status("Fetching", &format!("advisories `{}`", url), GREEN, true)
+        .unwrap();
 
-    let advisory_db = AdvisoryDatabase::fetch_from_url(url)
-        .expect("Couldn't fetch advisory database");
+    let advisory_db =
+        AdvisoryDatabase::fetch_from_url(url).expect("Couldn't fetch advisory database");
 
-    shell.say_status("Scanning",
-                    &format!("{} crates for vulnerabilities ({} advisories in database)",
-                             lockfile.packages.len(),
-                             advisory_db.iter().len()),
-                    GREEN,
-                    true)
+    shell
+        .say_status(
+            "Scanning",
+            &format!(
+                "{} crates for vulnerabilities ({} advisories in database)",
+                lockfile.packages.len(),
+                advisory_db.iter().len()
+            ),
+            GREEN,
+            true,
+        )
         .unwrap();
 
     let vulnerabilities = lockfile.vulnerabilities(&advisory_db);
 
     if vulnerabilities.is_empty() {
-        shell.say_status("Success", "No vulnerable packages found", GREEN, true)
+        shell
+            .say_status("Success", "No vulnerable packages found", GREEN, true)
             .unwrap();
     } else {
-        shell.say_status("Warning", "Vulnerable crates found!", RED, true)
+        shell
+            .say_status("Warning", "Vulnerable crates found!", RED, true)
             .unwrap()
     }
 
@@ -95,24 +114,35 @@ fn main() {
 }
 
 fn not_found(shell: &mut Shell, filename: &str) -> term::Result<()> {
-    shell.say_status("error:",
-                    format!("Couldn't find '{}'!", filename),
-                    RED,
-                    false)?;
-    shell.say("\nRun \"cargo build\" to generate lockfile before running audit",
-             WHITE)?;
+    shell.say_status(
+        "error:",
+        format!("Couldn't find '{}'!", filename),
+        RED,
+        false,
+    )?;
+    shell.say(
+        "\nRun \"cargo build\" to generate lockfile before running audit",
+        WHITE,
+    )?;
 
     Ok(())
 }
 
 fn vulns_found(shell: &mut Shell, vuln_count: usize) -> term::Result<()> {
     if vuln_count == 1 {
-        shell.say_status("\nerror:", "1 vulnerability found!", RED, false)?;
+        shell.say_status(
+            "\nerror:",
+            "1 vulnerability found!",
+            RED,
+            false,
+        )?;
     } else {
-        shell.say_status("\nerror:",
-                        format!("{} vulnerabilities found!", vuln_count),
-                        RED,
-                        false)?;
+        shell.say_status(
+            "\nerror:",
+            format!("{} vulnerabilities found!", vuln_count),
+            RED,
+            false,
+        )?;
     }
 
     Ok(())
