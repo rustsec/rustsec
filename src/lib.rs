affd60b16853363334f03cd03386ad8051a43c2e
Tony Arcieri <bascule@gmail.com>
2018-07-19 19:41:13 -0700

    Factor integration tests into the tests/ directory

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 //! rustsec: Client library for the `RustSec` security advisory database
+//!
+//! This crate is primarily intended for use with the cargo-audit tool:
+//!
+//! <https://crates.io/crates/cargo-audit>
 
 #![crate_name = "rustsec"]
 #![crate_type = "lib"]
@@ -31,45 +35,3 @@ pub use lockfile::Lockfile;
 /// URL where the TOML file containing the advisory database is located
 pub const ADVISORY_DB_URL: &str =
     "https://raw.githubusercontent.com/RustSec/advisory-db/master/Advisories.toml";
-
-#[cfg(test)]
-mod tests {
-    use db::AdvisoryDatabase;
-    use lockfile::Lockfile;
-    use semver::VersionReq;
-
-    // End-to-end integration test (has online dependency on GitHub)
-    #[test]
-    fn test_integration() {
-        let db = AdvisoryDatabase::fetch().unwrap();
-        let ref example_advisory = db.find("RUSTSEC-2017-0001").unwrap();
-
-        assert_eq!(example_advisory.id, "RUSTSEC-2017-0001");
-        assert_eq!(example_advisory.package, "sodiumoxide");
-        assert_eq!(
-            example_advisory.patched_versions[0],
-            VersionReq::parse(">= 0.0.14").unwrap()
-        );
-        assert_eq!(example_advisory.date, Some(String::from("2017-01-26")));
-        assert_eq!(
-            example_advisory.url,
-            Some(String::from(
-                "https://github.com/dnaq/sodiumoxide/issues/154"
-            ))
-        );
-        assert_eq!(
-            example_advisory.title,
-            "scalarmult() vulnerable to degenerate public keys"
-        );
-        assert_eq!(
-            &example_advisory.description[0..30],
-            "The `scalarmult()` function in"
-        );
-
-        let ref crate_advisories = db.find_by_crate("sodiumoxide");
-        assert_eq!(*example_advisory, crate_advisories[0]);
-
-        let lockfile = Lockfile::load("Cargo.lock").unwrap();
-        lockfile.vulnerabilities(&db);
-    }
-}
