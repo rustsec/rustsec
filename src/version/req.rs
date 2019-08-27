3ba0acc6cfb3757eed1e324fcf85047429f9d620
Tony Arcieri <bascule@gmail.com>
2019-08-26 21:32:44 -0700

    Improved handling of prereleases; MSRV 1.35+
    This change vendors a subset of the `VersionReq` functionality from the
    `semver` crate so it can have slightly different logic for prerelease
    matching from the `semver` crate. Context:
    
    https://github.com/steveklabnik/semver/issues/172
    
    The `VersionReq`s in this commit:
    
    - Treat prereleases as another dimension of major.minor.patch
      - Releases are treated as greater than prereleases
      - Prereleases are greater if their version number is greater
    - Disallow wildcards (instead we can use `patched_versions = []`)
    
    This includes a set of tests that should cover the relevant cases.
    
    Additionally bumps MSRV to 1.35.0. Technically we only need 1.34+ for
    this commit (which uses `TryFrom`), however `cargo-audit` is already
    using 1.35.0 so this bumps it to match.

---
+++ src/version/req.rs
@@ -0,0 +1,121 @@
+//! Version requirements
+//!
+//! The version matching needed by RustSec is slightly different from the
+//! logic needed by `semver` due to the handling of prerelease versions.
+//!
+//! See:
+//! - https://github.com/RustSec/cargo-audit/issues/17
+//! - https://github.com/RustSec/cargo-audit/issues/30
+//! - https://github.com/steveklabnik/semver/issues/172
+
+// Portions adapted from the `semver` crate.
+// Copyright (c) 2016 Steve Klabnik
+
+use super::{predicate::Predicate, Version};
+use crate::{Error, ErrorKind};
+use serde::{de, ser, Deserialize, Serialize};
+use std::{convert::TryFrom, fmt};
+
+/// Version requirements
+#[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+pub struct VersionReq {
+    predicates: Vec<Predicate>,
+}
+
+impl VersionReq {
+    /// Parse a version requirement from a string
+    pub fn parse(input: &str) -> Result<Self, Error> {
+        match semver_parser::range::parse(input) {
+            Ok(req) => Self::try_from(req),
+            Err(e) => fail!(ErrorKind::Version, "{}", e),
+        }
+    }
+
+    /// Match the given `Version` against this `VersionReq`
+    pub fn matches(&self, version: &Version) -> bool {
+        if self.predicates.is_empty() {
+            return true;
+        }
+
+        self.predicates.iter().all(|p| p.matches(version))
+    }
+}
+
+impl fmt::Display for VersionReq {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        if self.predicates.is_empty() {
+            write!(fmt, "*")?;
+        } else {
+            for (i, ref pred) in self.predicates.iter().enumerate() {
+                if i == 0 {
+                    write!(fmt, "{}", pred)?;
+                } else {
+                    write!(fmt, ", {}", pred)?;
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl TryFrom<semver_parser::range::VersionReq> for VersionReq {
+    type Error = Error;
+
+    fn try_from(other: semver_parser::range::VersionReq) -> Result<VersionReq, Error> {
+        let mut predicates = Vec::with_capacity(other.predicates.len());
+
+        for predicate in other.predicates.into_iter() {
+            predicates.push(Predicate::try_from(predicate)?);
+        }
+
+        Ok(VersionReq { predicates })
+    }
+}
+
+impl From<semver::VersionReq> for VersionReq {
+    fn from(version_req: semver::VersionReq) -> VersionReq {
+        Self::parse(&version_req.to_string()).unwrap()
+    }
+}
+
+impl From<VersionReq> for semver::VersionReq {
+    fn from(version_req: VersionReq) -> semver::VersionReq {
+        semver::VersionReq::parse(&version_req.to_string()).unwrap()
+    }
+}
+
+impl Serialize for VersionReq {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: ser::Serializer,
+    {
+        serializer.collect_str(self)
+    }
+}
+
+impl<'de> Deserialize<'de> for VersionReq {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: de::Deserializer<'de>,
+    {
+        struct VersionReqVisitor;
+
+        impl<'de> de::Visitor<'de> for VersionReqVisitor {
+            type Value = VersionReq;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("a SemVer version requirement as a string")
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                VersionReq::parse(v).map_err(de::Error::custom)
+            }
+        }
+
+        deserializer.deserialize_str(VersionReqVisitor)
+    }
+}
