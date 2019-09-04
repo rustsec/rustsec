ee8b2e5be22831593bf199941842df81b31e4944
Tony Arcieri <bascule@gmail.com>
2019-09-04 10:40:59 -0700

    Index the `rust` advisory directory from RustSec/advisory-db
    Adds initial support for indexing vulnerabilities in the Rust language
    itself, e.g. `std`, `rustdoc`
    
    These advisories are kept in a separate (new) package index and
    therefore ignored in all of the existing queries to find vulnerabilities
    based on the contents of the lockfile. However, they are exposed in the
    iterator over all of the vulnerabilities in the database, and therefore
    will end up getting rendered on https://rustsec.org, which is the
    immediate goal of indexing them.
    
    In the future we can look into potentially querying them based on e.g.
    the currently installed Rust toolchain.

--- src/package.rs
+++ src/package.rs
@@ -1,10 +1,13 @@
-//! Crate metadata as parsed from `Cargo.lock`
+//! Attributes of Rust packages
 
-use crate::version::Version;
-use serde::{Deserialize, Serialize};
-use std::fmt;
+use crate::{
+    error::{Error, ErrorKind},
+    version::Version,
+};
+use serde::{de, ser, Deserialize, Serialize};
+use std::{fmt, str::FromStr};
 
-/// A Rust package (i.e. crate) as structured in `Cargo.lock`
+/// Information about a Rust package (as sourced from `Cargo.lock`)
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct Package {
     /// Name of a crate
@@ -23,7 +26,7 @@ pub struct Package {
 
 /// Name of a Rust package
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, PartialOrd, Ord, Serialize)]
-pub struct Name(pub String);
+pub struct Name(String);
 
 impl Name {
     /// Get string reference to this package name
@@ -53,5 +56,81 @@ impl<'a> From<&'a str> for Name {
 impl Into<String> for Name {
     fn into(self) -> String {
         self.0
+    }
+}
+
+/// Collections of packages we collect advisories for
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq, PartialOrd, Ord)]
+pub enum Collection {
+    /// Crates published through crates.io
+    Crates,
+
+    /// Rust core vulnerabilities
+    Rust,
+}
+
+impl Collection {
+    /// Get a `str` representing the kind of package
+    pub fn as_str(&self) -> &str {
+        match self {
+            Collection::Crates => "crates",
+            Collection::Rust => "rust",
+        }
+    }
+}
+
+impl fmt::Display for Collection {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.as_str())
+    }
+}
+
+impl FromStr for Collection {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Error> {
+        Ok(match s {
+            "crates" => Collection::Crates,
+            "rust" => Collection::Rust,
+            other => fail!(ErrorKind::Parse, "invalid package type: {}", other),
+        })
+    }
+}
+
+impl<'de> Deserialize<'de> for Collection {
+    fn deserialize<D: de::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        use de::Error;
+        let string = String::deserialize(deserializer)?;
+        string.parse().map_err(D::Error::custom)
+    }
+}
+
+impl Serialize for Collection {
+    fn serialize<S: ser::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        self.to_string().serialize(serializer)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Collection;
+
+    #[test]
+    fn parse_crate() {
+        let crate_kind = "crates".parse::<Collection>().unwrap();
+        assert_eq!(Collection::Crates, crate_kind);
+        assert_eq!("crates", crate_kind.as_str());
+    }
+
+    #[test]
+    fn parse_rust() {
+        let rust_kind = "rust".parse::<Collection>().unwrap();
+        assert_eq!(Collection::Rust, rust_kind);
+        assert_eq!("rust", rust_kind.as_str());
+    }
+
+    #[test]
+    fn parse_other() {
+        assert!("foobar".parse::<Collection>().is_err());
     }
 }
