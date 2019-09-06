b861b49c4a35cb84f046a84edc53da647f814f22
Tony Arcieri <bascule@gmail.com>
2019-09-06 08:15:27 -0700

    Use index allocation for storing advisories
    Stores advisories in a `Vec` using a simple index allocation strategy,
    and has all database indexes refer to them by their "slot", i.e. offset
    into the advisory vector.

--- src/db/index.rs
+++ src/db/index.rs
@@ -2,11 +2,12 @@
 
 pub use crate::set::Iter;
 
-use crate::{advisory, map, package, Map, Set};
+use super::entries::Slot;
+use crate::{map, package, Map, Set};
 
 /// Database index which maps package names to a set of advisory IDs
 #[derive(Debug, Default)]
-pub(crate) struct Index(Map<package::Name, Set<advisory::Id>>);
+pub(crate) struct Index(Map<package::Name, Set<Slot>>);
 
 impl Index {
     /// Create a new index
@@ -15,17 +16,17 @@ impl Index {
     }
 
     /// Insert an entry into the index
-    pub fn insert(&mut self, key: &package::Name, value: &advisory::Id) -> bool {
+    pub fn insert(&mut self, key: &package::Name, slot: Slot) -> bool {
         let values = match self.0.entry(key.clone()) {
             map::Entry::Vacant(entry) => entry.insert(Set::new()),
             map::Entry::Occupied(entry) => entry.into_mut(),
         };
 
-        values.insert(value.clone())
+        values.insert(slot)
     }
 
     /// Get an iterator over advisory IDs for a given package name
-    pub fn get(&self, key: &package::Name) -> Option<Iter<'_, advisory::Id>> {
+    pub fn get(&self, key: &package::Name) -> Option<Iter<'_, Slot>> {
         self.0.get(key).map(|set| set.iter())
     }
 }
