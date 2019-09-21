f4f11f1542c798dd3eb2d4484cc041c7ddb96b52
Tony Arcieri <bascule@gmail.com>
2019-09-20 23:02:08 -0600

    Import implementation from the `rustsec` crate

---
+++ src/metadata.rs
@@ -0,0 +1,79 @@
+//! Package metadata
+
+use crate::error::Error;
+use serde::{de, ser, Deserialize, Serialize};
+use std::{
+    collections::BTreeMap as Map,
+    fmt::{self, Display},
+    str::FromStr,
+};
+
+/// Package metadata
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Metadata(Map<Checksum, Hash>);
+
+/// Package checksum info
+// TODO(tarcieri): properly parse checksum strings
+#[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+pub struct Checksum(String);
+
+impl Display for Checksum {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl FromStr for Checksum {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Error> {
+        Ok(Checksum(s.to_owned()))
+    }
+}
+
+impl<'de> Deserialize<'de> for Checksum {
+    fn deserialize<D: de::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        use de::Error;
+        let checksum = String::deserialize(deserializer)?;
+        checksum.parse().map_err(D::Error::custom)
+    }
+}
+
+impl Serialize for Checksum {
+    fn serialize<S: ser::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        self.to_string().serialize(serializer)
+    }
+}
+
+/// Package hashes
+// TODO(tarcieri): properly parse package hashes
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Hash(String);
+
+impl Display for Hash {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl FromStr for Hash {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Error> {
+        Ok(Hash(s.to_owned()))
+    }
+}
+
+impl<'de> Deserialize<'de> for Hash {
+    fn deserialize<D: de::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        use de::Error;
+        let checksum = String::deserialize(deserializer)?;
+        checksum.parse().map_err(D::Error::custom)
+    }
+}
+
+impl Serialize for Hash {
+    fn serialize<S: ser::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        self.to_string().serialize(serializer)
+    }
+}
