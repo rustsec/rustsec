332e316ad8321412c6840497d269c2d6a391d835
Tony Arcieri <bascule@gmail.com>
2019-09-23 17:24:03 -0700

    Refactor config and presenter logic
    This is a broad sweeping change that accomplishes the following:
    
    - Factor config apart into topic-related types
    - Factor all presentation logic into presenters, and factor apart
      the presentation logic related to dependency trees
    
    Together these should dramatically improve the quality of the codebase.

--- src/config.rs
+++ src/config.rs
@@ -15,58 +15,99 @@ use std::path::PathBuf;
 #[derive(Clone, Config, Debug, Default, Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct AuditConfig {
-    /// Path to the advisory database's git repo
-    pub advisory_db_path: Option<PathBuf>,
+    /// Advisory-related configuration
+    pub advisories: AdvisoryConfig,
 
-    /// URL to the advisory database
-    pub advisory_db_url: Option<String>,
+    /// Advisory Database configuration
+    pub database: DatabaseConfig,
 
-    /// Allow a stale advisory database?
-    pub allow_stale: bool,
+    /// Output configuration
+    pub output: OutputConfig,
 
-    /// Should colors be displayed?
-    pub color: Option<String>,
-
-    /// Vector of advisories to be ignored
-    #[serde(default)]
-    pub ignore: Vec<advisory::Id>,
-
-    /// Should we skip fetching the advisory database from git?
-    pub no_fetch: bool,
-
-    /// Output format to use
-    #[serde(default)]
-    pub output_format: OutputFormat,
-
-    /// Enable quiet mode
-    pub quiet: bool,
-
-    /// Ignore advisories with a severity lower than this threshold
-    pub severity_threshold: Option<advisory::Severity>,
-
-    /// Show information about dependency trees with vulnerabilities
-    pub show_dependency_tree: Option<bool>,
-
-    /// Target architecture to find vulnerabilities for
-    pub target_arch: Option<Arch>,
-
-    /// Target OS to find vulnerabilities for
-    pub target_os: Option<OS>,
+    /// Target-related configuration
+    pub target: TargetConfig,
 }
 
 impl AuditConfig {
     /// Get audit report settings from the configuration
     pub fn report_settings(&self) -> report::Settings {
         let mut settings = rustsec::report::Settings::default();
+        settings.ignore = self.advisories.ignore.clone();
+        settings.severity = self.advisories.severity_threshold;
+        settings.target_arch = self.target.arch;
+        settings.target_os = self.target.os;
 
-        // TODO(tarcieri): support for customizing informational advisory types to warn for
-        settings.informational_warnings = vec![advisory::Informational::Unmaintained];
-        settings.ignore = self.ignore.clone();
-        settings.severity = self.severity_threshold;
-        settings.target_arch = self.target_arch;
-        settings.target_os = self.target_os;
+        if let Some(informational_warnings) = &self.advisories.informational_warnings {
+            settings.informational_warnings = informational_warnings.clone();
+        } else {
+            // Alert for unmaintained packages by default
+            settings.informational_warnings = vec![advisory::Informational::Unmaintained];
+        }
 
         settings
+    }
+}
+
+/// Advisory-related configuration.
+#[derive(Clone, Config, Debug, Default, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct AdvisoryConfig {
+    /// Ignore advisories for the given IDs
+    #[serde(default)]
+    pub ignore: Vec<advisory::Id>,
+
+    /// Warn for the given types of informational advisories
+    pub informational_warnings: Option<Vec<advisory::Informational>>,
+
+    /// CVSS Qualitative Severity Rating Scale threshold to alert at.
+    ///
+    /// Vulnerabilities with explicit CVSS info which have a severity below
+    /// this threshold will be ignored.
+    pub severity_threshold: Option<advisory::Severity>,
+}
+
+/// Advisory Database configuration.
+///
+/// The advisory database is stored in a Git repository. This section of the
+/// configuration stores settings related to it.
+#[derive(Clone, Config, Debug, Default, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct DatabaseConfig {
+    /// Path to the local copy of advisory database's git repo (default: ~/.cargo/advisory-db)
+    pub path: Option<PathBuf>,
+
+    /// URL to the advisory database's git repo (default: https://github.com/RustSec/advisory-db)
+    pub url: Option<String>,
+
+    /// Perform a `git fetch` before auditing (default: true)
+    pub fetch: bool,
+
+    /// Allow a stale advisory database? (i.e. one which hasn't been updated in 90 days)
+    pub stale: bool,
+}
+
+/// Output configuration
+#[derive(Clone, Config, Debug, Default, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct OutputConfig {
+    /// Should colors be displayed?
+    pub color: Option<String>,
+
+    /// Output format to use
+    #[serde(default)]
+    pub format: OutputFormat,
+
+    /// Enable quiet mode
+    pub quiet: bool,
+
+    /// Show inverse dependency trees along with advisories (default: true)
+    pub show_tree: Option<bool>,
+}
+
+impl OutputConfig {
+    /// Is quiet mode enabled?
+    pub fn is_quiet(&self) -> bool {
+        self.quiet || self.format == OutputFormat::Json
     }
 }
 
@@ -86,4 +127,15 @@ impl Default for OutputFormat {
     fn default() -> Self {
         OutputFormat::Terminal
     }
+}
+
+/// Target configuration
+#[derive(Clone, Config, Debug, Default, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct TargetConfig {
+    /// Target architecture to find vulnerabilities for
+    pub arch: Option<Arch>,
+
+    /// Target OS to find vulnerabilities for
+    pub os: Option<OS>,
 }
