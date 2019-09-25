aaef9e8e4c8ba27bf09a16853eedc739c1d4aa79
Tony Arcieri <bascule@gmail.com>
2019-09-25 16:46:39 -0700

    v2.0.0-pre

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@
 
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustSec/logos/master/rustsec-logo-lg.png",
-    html_root_url = "https://docs.rs/cargo-lock/1.0.0"
+    html_root_url = "https://docs.rs/cargo-lock/2.0.0-pre"
 )]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
