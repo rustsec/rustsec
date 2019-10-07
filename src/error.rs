ae3162a7b7afb96295b9f3f3525f7321bd18d82c
Tony Arcieri <bascule@gmail.com>
2019-10-07 16:30:02 -0700

    .github: Use actions-rs GitHub Actions config

--- src/error.rs
+++ src/error.rs
@@ -18,7 +18,7 @@ macro_rules! format_err {
 /// Create and return an error with a formatted message
 macro_rules! fail {
     ($kind:path, $msg:expr) => {
-        Err(format_err!($kind, $msg))?
+        return Err(format_err!($kind, $msg));
     };
     ($kind:path, $fmt:expr, $($arg:tt)+) => {
         fail!($kind, format!($fmt, $($arg)+))
