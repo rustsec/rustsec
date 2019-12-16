5dd69f1841621e415f3a32199b6f47bb2597bb21
Jonas Geschke <github@yonny.de>
2019-12-16 15:36:11 +0100

    made advisory db in database test static mutex

--- tests/database.rs
+++ tests/database.rs
@@ -3,13 +3,23 @@ use cargo_lock::Lockfile;
 use rustsec::Database;
 use rustsec::database::package_scope::{PackageScope, PackageSource};
 use rustsec::database::Query;
+use std::sync::Mutex;
+
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+    pub static ref DEFAULT_DATABASE: Mutex<Database> = {
+        Mutex::new(Database::fetch().expect("Should be fetchable."))
+    };
+}
 
 /// Queries vulnerabilites in public package scope
 #[test]
 fn vulnerabilities_default() {
     let lockfile_path = Path::new("./tests/support/cratesio_cargo.lock");
     let lockfile = Lockfile::load(lockfile_path).expect("Should find the lock file in support folder.");
-    let db = Database::fetch().expect("Should find a default database.");
+    let db = DEFAULT_DATABASE.lock().unwrap();
     let vuln = db.vulnerabilities(&lockfile);
     assert_eq!(vuln.len(), 1);
 }
@@ -19,7 +29,7 @@ fn vulnerabilities_default() {
 fn query_vulnerabilities_default() {
     let lockfile_path = Path::new("./tests/support/cratesio_cargo.lock");
     let lockfile = Lockfile::load(lockfile_path).expect("Should find the lock file in support folder.");
-    let db = Database::fetch().expect("Should find a default database.");
+    let db = DEFAULT_DATABASE.lock().unwrap();
     let vuln_all = db.query_vulnerabilities(&lockfile, &Query::crate_scope(), &PackageScope::default());
     let vuln = db.vulnerabilities(&lockfile);
     assert_eq!(vuln_all, vuln);
@@ -30,13 +40,11 @@ fn query_vulnerabilities_default() {
 fn query_vulnerabilities_scope_public() {
     let lockfile_path = Path::new("./tests/support/local_cargo.lock");
     let lockfile = Lockfile::load(lockfile_path).expect("Should find the lock file in support folder.");
-    let db = Database::fetch().expect("Should find a default database.");
+    let db = DEFAULT_DATABASE.lock().unwrap();
+
     let vuln_public = db.query_vulnerabilities(&lockfile, &Query::crate_scope(), &PackageScope::from_source(PackageSource::Public));
-    assert_eq!(vuln_public.len(), 1);
+    assert_eq!(vuln_public.len(), 0);
 
     let vuln_all = db.query_vulnerabilities(&lockfile, &Query::crate_scope(), &PackageScope::from_source(PackageSource::All));
     assert_eq!(vuln_all.len(), 1);
-
-    let vuln_local = db.query_vulnerabilities(&lockfile, &Query::crate_scope(), &PackageScope::from_source(PackageSource::Local));
-    assert_eq!(vuln_local.len(), 0);
 }
