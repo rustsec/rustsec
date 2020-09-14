79cc48c373eb6b281fbfa5fb4c028e38eac853e2
Tony Arcieri <bascule@gmail.com>
2020-09-14 08:55:45 -0700

    .github: add 'override: true' directives; MSRV 1.46+ (#26)
    It seems in absence of the `override: true`, the tests were always
    running against the latest stable Rust.

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@
 //!
 //! ## Minimum Supported Rust Version
 //!
-//! Rust **1.40** or higher.
+//! Rust **1.46** or higher.
 //!
 //! Minimum supported Rust version can be changed in the future, but it will be
 //! done with a minor version bump.
