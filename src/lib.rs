08c9f3e2f9a8aa0e140bee1b9007220d7b750fd8
Tony Arcieri <bascule@gmail.com>
2021-03-07 18:48:09 -0800

    v0.14.0 (#330)

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@
 
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustSec/logos/main/rustsec-logo-lg.png",
-    html_root_url = "https://docs.rs/cargo-audit/0.13.0"
+    html_root_url = "https://docs.rs/cargo-audit/0.14.0"
 )]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms, trivial_casts, unused_qualifications)]
