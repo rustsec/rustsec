15da0293620cca4658b548e0e0b41050c35002cb
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2021-05-21 19:39:32 +0200

    Better error reporting

--- admin/src/error.rs
+++ admin/src/error.rs
@@ -66,8 +66,8 @@ impl From<Context<ErrorKind>> for Error {
     }
 }
 
-impl From<ureq::Error> for Error {
-    fn from(other: ureq::Error) -> Self {
+impl From<crates_index::Error> for Error {
+    fn from(other: crates_index::Error) -> Self {
         format_err!(ErrorKind::CratesIo, "{}", other).into()
     }
 }
