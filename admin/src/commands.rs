d87f3ef4688d122dbb3a7ca58ddba8de2f0d3dcf
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2021-05-22 13:39:04 +0200

    Implement list-affected-versions subcommand, works fine with current DB

--- admin/src/commands.rs
+++ admin/src/commands.rs
@@ -2,11 +2,14 @@
 
 mod assign_id;
 mod lint;
-mod version;
 mod list_affected_versions;
+mod version;
 mod web;
 
-use self::{assign_id::AssignIdCmd, lint::LintCmd, version::VersionCmd, web::WebCmd, list_affected_versions::ListAffectedVersionsCmd};
+use self::{
+    assign_id::AssignIdCmd, lint::LintCmd, list_affected_versions::ListAffectedVersionsCmd,
+    version::VersionCmd, web::WebCmd,
+};
 use crate::config::AppConfig;
 use abscissa_core::{Command, Configurable, Help, Options, Runnable};
 use std::path::PathBuf;
--- admin/src/commands/list_affected_versions.rs
+++ admin/src/commands/list_affected_versions.rs
@@ -3,17 +3,58 @@
 //! Can be used to verify that the version specification in the advisory
 //! had the desired effect and matches only the versions you want it to
 
-
 #![allow(clippy::never_loop)]
+#![allow(unused_variables)] //TODO
+
+use std::{
+    path::{Path, PathBuf},
+    process::exit,
+};
 
 use abscissa_core::{Command, Options, Runnable};
 
+use crate::list_versions;
+use crate::prelude::*;
+
 /// `rustsec-admin list-affected-versions` subcommand
 #[derive(Command, Debug, Default, Options)]
-pub struct ListAffectedVersionsCmd {}
+pub struct ListAffectedVersionsCmd {
+    /// Path to the advisory database
+    #[options(free, help = "filesystem path to the RustSec advisory DB git repo")]
+    path: Vec<PathBuf>,
+}
 
 impl Runnable for ListAffectedVersionsCmd {
     fn run(&self) {
-        println!("hello world");
+        let repo_path = match self.path.len() {
+            0 => Path::new("."),
+            1 => self.path[0].as_path(),
+            _ => Self::print_usage_and_exit(&[]),
+        };
+
+        let advisory_db = rustsec::Database::open(&repo_path).unwrap_or_else(|e| {
+            status_err!(
+                "Failed to open advisory database at {}: {}",
+                repo_path.display(),
+                e
+            );
+            exit(1);
+        });
+
+        // Ensure we're parsing some advisories
+        let advisories = advisory_db.iter();
+        if advisories.len() == 0 {
+            status_err!("no advisories found in {}", repo_path.display());
+            exit(1);
+        }
+
+        list_versions::do_it(advisory_db).unwrap_or_else(|e| {
+            status_err!(
+                "error listing affected versions for DB {}: {}",
+                repo_path.display(),
+                e
+            );
+            exit(1);
+        });
     }
 }
