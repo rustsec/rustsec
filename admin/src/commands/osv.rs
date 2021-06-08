3bc900e280a51ea6208e02fc57f2aaee76fe5d14
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2021-06-08 20:43:20 +0200

    Basic scaffolding for CLI export to OSV

--- admin/src/commands/osv.rs
+++ admin/src/commands/osv.rs
@@ -1,11 +1,27 @@
 //! `rustsec-admin osv` subcommand
+//!
+//! Exports all advisories to the OSV format defined at
+//! https://github.com/google/osv
+
+#![allow(unused_variables)] //TODO
+
+use std::path::{Path, PathBuf};
 
 use abscissa_core::{Command, Options, Runnable};
 #[derive(Command, Debug, Default, Options)]
-pub struct OsvCmd {}
+pub struct OsvCmd {
+    /// Path to the advisory database
+    #[options(free, help = "filesystem path to the RustSec advisory DB git repo")]
+    path: Vec<PathBuf>,
+}
 
 impl Runnable for OsvCmd {
     fn run(&self) {
-        println!("hello world");
+        let repo_path = match self.path.len() {
+            0 => Path::new("."),
+            1 => self.path[0].as_path(),
+            _ => Self::print_usage_and_exit(&[]),
+        };
+
     }
 }
