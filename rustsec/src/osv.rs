ce5fa25a6dfef5e7d2d79bb653faafd0093a5334
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2021-06-08 20:42:59 +0200

    Checking in something I forgot to check in

---
+++ rustsec/src/osv.rs
@@ -0,0 +1,17 @@
+//! Provides support for exporting to the interchange format defined by
+//! https://github.com/google/osv
+//!
+//! We also use OSV-style ranges for version matching in RustSec crate
+//! because it allows handling pre-releases correctly,
+//! which `semver` crate does not allow doing directly.
+//! See https://github.com/dtolnay/semver/issues/172
+
+// These are implementation details and are not exported outside OSV module
+mod osv_advisory;
+mod osv_range;
+mod range_conversion;
+mod unaffected_range;
+
+pub use osv_advisory::OsvAdvisory;
+pub use osv_range::OsvRange;
+pub use range_conversion::ranges_for_advisory;
