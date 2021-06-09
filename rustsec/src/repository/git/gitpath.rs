181f3d5db010d8c513168a6a8ce379459d7ac1ca
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2021-06-09 23:14:06 +0200

    More concise error handling

--- rustsec/src/repository/git/gitpath.rs
+++ rustsec/src/repository/git/gitpath.rs
@@ -16,11 +16,11 @@ impl<'a> GitPath<'a> {
     pub fn new(repo: &'a Repository, path: &'a Path) -> Result<Self, Error> {
         // Validate that the path is relative for better feedback to API users
         if path.has_root() {
-            return Err(format_err!(
+            fail!(
                 ErrorKind::BadParam,
                 "{} is not a relative path",
                 path.display()
-            ));
+            );
         }
         let commit_id = repo.repo.refname_to_id("HEAD")?;
         let commit = repo.repo.find_commit(commit_id)?;
