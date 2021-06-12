6ea95b960856b51144b22f2a0bb1bad626aceee1
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2021-06-12 03:54:55 +0200

    panic instead of misbehaving silently if an invalid char is passed to next_valid_char()

--- rustsec/src/osv/range_conversion.rs
+++ rustsec/src/osv/range_conversion.rs
@@ -208,7 +208,8 @@ const ORDERED_VALID_CHARS: [char; 64] = [
 const INDEX_FOR_CHAR: [Option<usize>; 123] = [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, Some(0), None, None, Some(1), Some(2), Some(3), Some(4), Some(5), Some(6), Some(7), Some(8), Some(9), Some(10), None, None, None, None, None, None, None, Some(11), Some(12), Some(13), Some(14), Some(15), Some(16), Some(17), Some(18), Some(19), Some(20), Some(21), Some(22), Some(23), Some(24), Some(25), Some(26), Some(27), Some(28), Some(29), Some(30), Some(31), Some(32), Some(33), Some(34), Some(35), Some(36), None, None, None, None, Some(37), None, Some(38), Some(39), Some(40), Some(41), Some(42), Some(43), Some(44), Some(45), Some(46), Some(47), Some(48), Some(49), Some(50), Some(51), Some(52), Some(53), Some(54), Some(55), Some(56), Some(57), Some(58), Some(59), Some(60), Some(61), Some(62), Some(63)];
 
 fn next_valid_char(current: char) -> Option<char> {
-    let idx = INDEX_FOR_CHAR.get(current as usize)?.clone()?;
+    // panic instead of misbehaving silently if an invalid char is passed
+    let idx = INDEX_FOR_CHAR[current as usize].unwrap();
     ORDERED_VALID_CHARS.get(idx + 1).copied()
 }
 
