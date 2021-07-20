8744816aa77f454e4a88ec63fac43a25a5353b21
Tony Arcieri <bascule@gmail.com>
2021-07-20 07:35:24 -0700

    rustsec v0.24.2 (#404)

--- rustsec/src/lib.rs
+++ rustsec/src/lib.rs
@@ -6,7 +6,7 @@
 
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustSec/logos/main/rustsec-logo-lg.png",
-    html_root_url = "https://docs.rs/rustsec/0.24.1"
+    html_root_url = "https://docs.rs/rustsec/0.24.2"
 )]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
