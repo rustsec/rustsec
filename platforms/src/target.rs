a7605cf69674301391ac13d07675088e60ba26a1
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-03-04 14:45:03 +0100

    cargo fmt

--- platforms/src/target.rs
+++ platforms/src/target.rs
@@ -4,15 +4,9 @@
 //! <https://doc.rust-lang.org/reference/attributes.html#conditional-compilation>
 
 mod arch;
+mod endian;
 mod env;
 mod os;
 mod pointerwidth;
-mod endian;
 
-pub use self::{
-    arch::Arch,
-    env::Env,
-    os::OS,
-    pointerwidth::PointerWidth,
-    endian::Endian,
-};
+pub use self::{arch::Arch, endian::Endian, env::Env, os::OS, pointerwidth::PointerWidth};
