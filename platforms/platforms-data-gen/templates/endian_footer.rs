d6f2305dc31ce1c9b47f9a308449b7adf5b33432
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-03-04 10:32:59 +0100

    Populate more templates

--- platforms/platforms-data-gen/templates/endian_footer.rs
+++ platforms/platforms-data-gen/templates/endian_footer.rs
@@ -1,22 +1,22 @@
 
-impl fmt::Display for Env {
+impl fmt::Display for Endian {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str(self.as_str())
     }
 }
 
 #[cfg(feature = "serde")]
-impl Serialize for Env {
+impl Serialize for Endian {
     fn serialize<S: ser::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
         serializer.serialize_str(self.as_str())
     }
 }
 
 #[cfg(feature = "serde")]
-impl<'de> Deserialize<'de> for Env {
+impl<'de> Deserialize<'de> for Endian {
     fn deserialize<D: de::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
         Ok(<&str>::deserialize(deserializer)?
             .parse()
-            .unwrap_or(Env::Unknown))
+            .unwrap_or(Endian::Unknown))
     }
 }
