1a566b0ca08c4186dbcf503bbe2d63000feb5175
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-03-04 10:55:55 +0100

    Fix swapped header and footer for Bits, why is this so fiddly?

---
+++ platforms/platforms-data-gen/templates/bits_header.rs
@@ -0,0 +1,12 @@
+//! Pointer width of the target architecture
+
+use crate::error::Error;
+use core::{fmt, str::FromStr};
+
+#[cfg(feature = "serde")]
+use serde::{de, ser, Deserialize, Serialize};
+
+/// `target_pointer_width`: Size of native pointer types (`usize`, `isize`) in bits
+/// 64 bits for modern desktops and phones, 32-bits for older devices, 16 bits for certain microcontrollers
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq, PartialOrd, Ord)]
+#[non_exhaustive]
