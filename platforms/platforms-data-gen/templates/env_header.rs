4ebf4213b9da32b7f603968a51b9efa5560539bd
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-03-04 09:46:28 +0100

    Add header and footer files for enums

---
+++ platforms/platforms-data-gen/templates/env_header.rs
@@ -0,0 +1,14 @@
+//! Rust target environments
+
+use crate::error::Error;
+use core::{fmt, str::FromStr};
+
+#[cfg(feature = "serde")]
+use serde::{de, ser, Deserialize, Serialize};
+
+/// `target_env`: target enviroment that disambiguates the target platform by ABI / libc.
+/// This value is closely related to the fourth element of the platform target triple,
+/// though it is not identical. For example, embedded ABIs such as `gnueabihf` will simply
+/// define `target_env` as `"gnu"` (i.e. `target::Env::GNU`)
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq, PartialOrd, Ord)]
+#[non_exhaustive]
