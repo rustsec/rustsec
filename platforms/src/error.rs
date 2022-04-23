d58fb56147e3fe23bbb5b6f433f9fbcb6a86e129
Tony Arcieri <bascule@gmail.com>
2022-04-23 12:34:55 -0600

    platforms v3.0.0

--- platforms/src/error.rs
+++ platforms/src/error.rs
@@ -2,18 +2,15 @@
 
 use core::fmt::{self, Display};
 
-#[cfg(feature = "std")]
-use std::error::Error as StdError;
-
 /// Error type
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Error;
 
 impl Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str("platforms::Error")
+        f.write_str("platforms error")
     }
 }
 
 #[cfg(feature = "std")]
-impl StdError for Error {}
+impl std::error::Error for Error {}
