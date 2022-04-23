7b28351ac80e7f8d271e83045cd1c2976dd09e77
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2022-04-23 17:55:51 +0200

    Split comments on enums into header and content for easier reading on docs.rs

--- platforms/platforms-data-gen/templates/os_header.rs
+++ platforms/platforms-data-gen/templates/os_header.rs
@@ -6,7 +6,9 @@ use core::{fmt, str::FromStr};
 #[cfg(feature = "serde")]
 use serde::{de, ser, de::Error as DeError, Deserialize, Serialize};
 
-/// `target_os`: Operating system of the target. This value is closely related to the second
-/// and third element of the platform target triple, though it is not identical.
+/// `target_os`: Operating system of the target.
+///
+/// This value is closely related to the second and third element
+/// of the platform target triple, though it is not identical.
 #[derive(Copy, Clone, Debug, Eq, Hash, PartialEq, PartialOrd, Ord)]
 #[non_exhaustive]
