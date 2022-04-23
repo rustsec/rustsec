c2a224b2774604dbad9eb640e3297e0727be93f4
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2022-04-23 17:56:23 +0200

    Regenerate `platforms` crate

--- platforms/src/target/os.rs
+++ platforms/src/target/os.rs
@@ -6,8 +6,10 @@ use core::{fmt, str::FromStr};
 #[cfg(feature = "serde")]
 use serde::{de, de::Error as DeError, ser, Deserialize, Serialize};
 
-/// `target_os`: Operating system of the target. This value is closely related to the second
-/// and third element of the platform target triple, though it is not identical.
+/// `target_os`: Operating system of the target.
+///
+/// This value is closely related to the second and third element
+/// of the platform target triple, though it is not identical.
 #[derive(Copy, Clone, Debug, Eq, Hash, PartialEq, PartialOrd, Ord)]
 #[non_exhaustive]
 pub enum OS {
