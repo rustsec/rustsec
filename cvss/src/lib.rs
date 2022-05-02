99a1a1835f6bde5acffeb615f5875baa83cb1e61
Tony Arcieri <bascule@gmail.com>
2022-05-01 18:01:58 -0600

    cvss: upgrade to 2021 edition; MSRV 1.56 (#547)

--- cvss/src/lib.rs
+++ cvss/src/lib.rs
@@ -1,22 +1,20 @@
-//! Common Vulnerability Scoring System.
-//!
-//! The [`cvss::v3::Base`][`v3::Base`] type provides the main functionality
-//! currently implemented by this crate, namely: support for parsing, serializing,
-//! and scoring `CVSS:3.0` and `CVSS:3.1` Base Metric Group vector strings
-//! as described in the CVSS v3.1 Specification:
-//!
-//! <https://www.first.org/cvss/specification-document>
-//!
-//! Serde support is available through the optional `serde` Cargo feature.
-
-// TODO(tarcieri): other CVSS versions, CVSS v3.1 Temporal and Environmental Groups
-
-#![doc(
-    html_logo_url = "https://raw.githubusercontent.com/RustSec/logos/main/rustsec-logo-lg.png",
-    html_root_url = "https://docs.rs/cvss/1.0.2"
-)]
+#![doc = include_str!("../README.md")]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/RustSec/logos/main/rustsec-logo-lg.png")]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
+
+//! ## Usage
+//!
+//! The [`v3::Base`] type provides the main functionality currently implemented
+//! by this crate, namely: support for parsing, serializing, and scoring
+//! `CVSS:3.0` and `CVSS:3.1` Base Metric Group vector strings as described in
+//! the [CVSS v3.1 Specification].
+//!
+//! Serde support is available through the optional `serde` Cargo feature.
+//!
+//! [CVSS v3.1 Specification]: https://www.first.org/cvss/specification-document
+
+// TODO(tarcieri): other CVSS versions, CVSS v3.1 Temporal and Environmental Groups
 
 pub mod severity;
 
