eb71773b9fe4b82eea36a738554895c5798dbaea
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2022-08-10 20:43:24 +0200

    Move the error message selection logic from cargo-audit to rustsec crate

--- cargo-audit/src/auditor.rs
+++ cargo-audit/src/auditor.rs
@@ -221,12 +221,7 @@ impl Auditor {
             input
         };
         // Extract the compressed audit data
-        let compressed_audit_data = auditable_extract::raw_auditable_data(&binary).map_err(|err| {
-            match err {
-                auditable_extract::Error::NoAuditData => Error::new(ErrorKind::Parse, &"No audit data found! Was the binary built with 'cargo auditable'?"),
-                other_err => Error::from(other_err),
-            }
-        })?;
+        let compressed_audit_data = auditable_extract::raw_auditable_data(&binary)?;
         // Decompress with a 8MiB size limit. Audit data JSONs don't get that large; anything this big is a DoS attempt
         let text = decompress_to_vec_zlib_with_limit(compressed_audit_data, 1024 * 1024 * 8)?;
         let json_structs: auditable_serde::VersionInfo = serde_json::from_slice(&text)?;
