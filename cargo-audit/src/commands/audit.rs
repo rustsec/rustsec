e63fc1dffd607fa2fdeb89598cc39b1476bf0de6
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2022-09-29 03:48:10 +0200

    cargo fmt

--- cargo-audit/src/commands/audit.rs
+++ cargo-audit/src/commands/audit.rs
@@ -148,7 +148,8 @@ pub enum AuditSubcommand {
 impl AuditCommand {
     /// Get the color configuration
     pub fn color_config(&self) -> Option<ColorChoice> {
-        #[allow(unused_mut)] // because it triggers a warning with the `binary-scanning` feature disabled
+        // suppress the warning that occurs with the `binary-scanning` feature disabled
+        #[allow(unused_mut)]
         let mut raw_color_setting = self.color.as_ref();
         #[cfg(feature = "binary-scanning")]
         if let Some(AuditSubcommand::Bin(ref command)) = self.subcommand {
