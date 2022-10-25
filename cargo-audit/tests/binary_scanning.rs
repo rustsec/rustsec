6325ba19bc42065a9843b87f66e09e988b97dc02
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-10-26 01:10:46 +0200

    Add tests for panic-based version detection

--- cargo-audit/tests/binary_scanning.rs
+++ cargo-audit/tests/binary_scanning.rs
@@ -46,10 +46,17 @@ fn cmd_runner() -> CmdRunner {
 }
 
 #[test]
-fn plain_binary_without_audit_info_passes() {
+fn panicking_binary_without_vulnerabilities_passes() {
     let mut binary_path = binaries_dir();
     binary_path.push("binary-without-audit-info");
     assert_eq!(cmd_runner().arg(binary_path).status().code(), 0);
+}
+
+#[test]
+fn panicking_binary_with_vulnerabilities_fails() {
+    let mut binary_path = binaries_dir();
+    binary_path.push("binary-with-vuln-panic");
+    assert_eq!(cmd_runner().arg(binary_path).status().code(), 1);
 }
 
 #[test]
