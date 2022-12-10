8d5eaa039ac2ad68389124e33b4c648444a1a8ab
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-12-11 00:00:03 +0100

    Add glob support on Windows via `wild` crate

--- admin/src/bin/rustsec-admin/main.rs
+++ admin/src/bin/rustsec-admin/main.rs
@@ -7,5 +7,11 @@ use rustsec_admin::application::APPLICATION;
 
 /// Boot the `rustsec-admin` CLI application
 fn main() {
-    abscissa_core::boot(&APPLICATION);
+    // copied from `abscissa` code:
+    // https://github.com/iqlusioninc/abscissa/blob/7c9ab7976145d2920a99ef00eba845efab3247a1/core/src/application.rs#L171-L177
+    // so that we could inject wild::args() instead of std::env::args()
+    // which provides glob expansion on Windows
+    let args = std::env::args();
+    abscissa_core::application::Application::run(&APPLICATION, args);
+    std::process::exit(0);
 }
