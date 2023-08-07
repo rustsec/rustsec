3b2f871d27ea97a5857c9f4ccc77fff6018bb2f1
Jake Shadle <jake.shadle@embark-studios.com>
2023-08-07 09:59:13 +0200

    Checkpoint

--- rustsec/src/repository/git/commit.rs
+++ rustsec/src/repository/git/commit.rs
@@ -2,12 +2,12 @@
 
 use crate::{
     error::{Error, ErrorKind},
-    repository::{git::Repository, signature::Signature},
+    repository::{
+        git::{self, Repository},
+        signature::Signature,
+    },
 };
-use std::{
-    ops::Deref,
-    time::{Duration, SystemTime, UNIX_EPOCH},
-};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 /// Number of days after which the repo will be considered stale
 /// (90 days)
@@ -27,7 +27,7 @@ pub struct Commit {
     pub summary: String,
 
     /// Commit time in number of seconds since the UNIX epoch
-    pub timestamp: SystemTime,
+    pub timestamp: time::OffsetDateTime,
 
     /// Signature on the commit (mandatory for Repository::fetch)
     // TODO: actually verify signatures
@@ -40,15 +40,70 @@ pub struct Commit {
 impl Commit {
     /// Get information about HEAD
     pub(crate) fn from_repo_head(repo: &Repository) -> Result<Self, Error> {
-        let head = repo.repo.head()?;
+        let find_remote_head = || -> Result<(gix::Commit<'_>, time::OffsetDateTime), Error> {
+            const CANDIDATE_REFS: &[&str] = &[
+                "FETCH_HEAD",  /* the location with the most-recent updates, as written by gix/us */
+                "origin/HEAD", /* typical refspecs update this symbolic ref to point to the actual remote ref with the fetched commit */
+                "origin/main", /* for good measure, resolve this branch by hand in case origin/HEAD is broken */
+                "HEAD", /* last resort, this would only be needed for a fresh clone via git/git2 */
+            ];
+            let mut candidates: Vec<_> = CANDIDATE_REFS
+                .iter()
+                .enumerate()
+                .filter_map(|(i, refname)| {
+                    let ref_id = repo
+                        .repo
+                        .find_reference(*refname)
+                        .ok()?
+                        .into_fully_peeled_id()
+                        .ok()?;
 
-        let oid = head.target().ok_or_else(|| {
-            format_err!(
-                ErrorKind::Repo,
-                "no ref target for: {}",
-                repo.path.display()
-            )
-        })?;
+                    let commit = ref_id.object().ok()?.try_into_commit().ok()?;
+                    let commit_time = commit.time().ok()?;
+
+                    Some((i, commit, commit_time))
+                })
+                .collect();
+
+            // Sort from oldest to newest, the last one will be the best reference
+            // we could reasonably locate, and since we are on second resolution,
+            // prefer the ordering of candidates if times are equal (can happen during eg testing)
+            //
+            // This allows FETCH_HEAD to be authoritative, unless one of the other
+            // references is more up to date, which can occur in (at least) 2 scenarios:
+            //
+            // 1. The repo is a fresh clone by cargo either via git or libgit2,
+            // neither of which write FETCH_HEAD during clone
+            // 2. A fetch was performed by an external crate/program to
+            // ourselves that didn't update FETCH_HEAD
+            candidates.sort_by(|a, b| match a.2.seconds.cmp(&b.2.seconds) {
+                std::cmp::Ordering::Equal => b.0.cmp(&a.0),
+                o => o,
+            });
+
+            // get the most recent commit, the one with most time passed since unix epoch.
+            let best = candidates.last().ok_or_else(|| {
+                format_err!(ErrorKind::Repo, "unable to find a suitable HEAD commit")
+            })?;
+
+            // In case we used FETCH_HEAD, use the mtime of the file itself, as
+            // it will be updated on every (successful) fetch and is a true
+            // time of the last update, rather than just the time of the last
+            // remote commit
+            let commit_time = if best.0 == 0 {
+                std::fs::metadata(repo.repo.path().join("FETCH_HEAD"))
+                    .ok()
+                    .and_then(|md| md.modified().ok().map(|t| t.into()))
+            } else {
+                None
+            };
+
+            let commit_time = commit_time.unwrap_or_else(|| git::gix_time_to_time(best.2));
+
+            Ok((best.1, commit_time))
+        };
+
+        let (commit, commit_time) = find_remote_head()?;
 
         let commit_id = oid.to_string();
         let commit_object = repo.repo.find_object(oid, Some(git2::ObjectType::Commit))?;
@@ -67,17 +122,18 @@ impl Commit {
             _ => (None, None),
         };
 
-        let time = UNIX_EPOCH + Duration::from_secs(commit.time().seconds() as u64);
-
-        Ok(Commit {
+        Ok(Self {
             commit_id,
             author,
             summary,
-            timestamp: time,
+            timestamp: commit_time,
             signature,
             signed_data,
         })
     }
+
+    /// Finds the most appropriate head commit for the repo
+    fn find_head(repo: &Repository) -> Result<(&'static str,), Error> {}
 
     /// Is the commit timestamp "fresh" as in the database has been updated
     /// recently? (i.e. 90 days, per the `STALE_AFTER` constant)
