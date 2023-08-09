0805822cbcf56cbbf1ec00c365880ccbf8bc5f47
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2023-08-09 20:53:36 +0200

    Drop the interrupt handler because it appears to be unnecessary in my testing

--- rustsec/src/repository/git/repository.rs
+++ rustsec/src/repository/git/repository.rs
@@ -70,10 +70,6 @@ impl Repository {
             fs::remove_dir(&path)?;
         }
 
-        // Set up signal handlers so that the lock is released if the user presses Ctrl+C
-        // see https://github.com/rustsec/rustsec/pull/925#discussion_r1287265212
-        gix::interrupt::init_handler(||{}).unwrap();
-
         // Lock the directory with the git repo checkout so that several processes don't trample each other
         const LOCK_WAIT_MINUTES: u64 = 10;
         let _lock = {
