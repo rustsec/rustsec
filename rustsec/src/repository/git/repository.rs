85799e9ce546d3b63c555c3e90dfb5d2006c25c6
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2023-08-17 18:37:43 +0200

    Fix doc links

--- rustsec/src/repository/git/repository.rs
+++ rustsec/src/repository/git/repository.rs
@@ -42,7 +42,7 @@ impl Repository {
     ///
     /// ## Locking
     /// This function will wait for up to 5 minutes for the filesystem lock on the repository.
-    /// It will fail with [`rustsec::Error::LockTimeout`](rustsec::Error) if the lock is still held
+    /// It will fail with [`rustsec::Error::LockTimeout`](Error) if the lock is still held
     /// after that time. Use [Repository::fetch] if you need to configure locking behavior.
     ///
     /// Regardless of the timeout, this function relies on `panic = unwind` to avoid leaving stale locks
@@ -62,7 +62,7 @@ impl Repository {
     /// ## Locking
     ///
     /// This function will wait for up to `lock_timeout` for the filesystem lock on the repository.
-    /// It will fail with [`rustsec::Error::LockTimeout`](rustsec::Error) if the lock is still held
+    /// It will fail with [`rustsec::Error::LockTimeout`](Error) if the lock is still held
     /// after that time.
     ///
     /// If `lock_timeout` is set to `std::time::Duration::from_secs(0)`, it will not wait at all,
