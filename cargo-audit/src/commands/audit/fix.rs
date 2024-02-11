d2b32f42f18b06e17db488d3ba2f19c5fa3b95fa
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-02-11 20:58:29 +0000

    WIP conversion of cargo-audit to the new rustsec fixer API

--- cargo-audit/src/commands/audit/fix.rs
+++ cargo-audit/src/commands/audit/fix.rs
@@ -2,6 +2,7 @@
 
 use crate::{auditor::Auditor, lockfile, prelude::*};
 use abscissa_core::{Command, Runnable};
+use cargo_lock::Lockfile;
 use clap::Parser;
 use rustsec::Fixer;
 use std::{
@@ -60,14 +61,11 @@ impl Runnable for FixCommand {
             }
         };
 
-        let mut fixer = Fixer::new(self.cargo_toml_path()).unwrap_or_else(|e| {
-            status_err!(
-                "couldn't load manifest from {}: {}",
-                self.cargo_toml_path().display(),
-                e
-            );
-            exit(1);
-        });
+        // This should always succeed because the auditor loaded it successfully already
+        let lockfile =
+            Lockfile::load("tests/examples/Cargo.lock").expect("Failed to load Cargo.lock");
+
+        let fixer = Fixer::new(self.cargo_toml_path(), lockfile);
 
         let dry_run = self.dry_run;
         let dry_run_info = if dry_run { " (dry run)" } else { "" };
@@ -80,8 +78,16 @@ impl Runnable for FixCommand {
         );
 
         for vulnerability in &report.vulnerabilities.list {
-            if let Err(e) = fixer.fix(vulnerability, dry_run) {
-                status_warn!("{}", e);
+            let results = fixer.fix(vulnerability, dry_run);
+            for outcome in results.outcomes {
+                match outcome.output {
+                    Ok(_) => todo!(),
+                    Err(e) => status_warn!(
+                        "Failed to run `cargo update` for package {}: {}",
+                        &outcome.package.name,
+                        e
+                    ),
+                }
             }
         }
 
