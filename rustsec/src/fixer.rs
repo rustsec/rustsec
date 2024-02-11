317b20f5383ee255c65c058ce2a76cab821d8d64
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-02-11 19:10:31 +0000

    WIP

--- rustsec/src/fixer.rs
+++ rustsec/src/fixer.rs
@@ -14,7 +14,7 @@ pub struct Fixer<'a> {
     lockfile: &'a Lockfile,
 }
 
-impl <'a> Fixer<'a> {
+impl<'a> Fixer<'a> {
     /// Create a new [`Fixer`] for the given `Cargo.toml` file
     pub fn new(cargo_toml: &'a Path, cargo_lock: &'a Lockfile) -> Self {
         Self {
@@ -24,11 +24,13 @@ impl <'a> Fixer<'a> {
     }
 
     /// Attempt to fix the given vulnerability
-    pub fn fix(
-        &self,
-        vulnerability: &Vulnerability,
-        dry_run: bool,
-    ) -> Result<(), Error> {
+    pub fn fix(&self, vulnerability: &Vulnerability, dry_run: bool) -> Result<(), Error> {
+        let cargo_path = std::env::var_os("CARGO").unwrap_or("cargo".into());
+        let pkg_name = &vulnerability.package.name;
+        // there can be more than one version of a given package in the lockfile, so we need to iterate over all of them
+        for pkg in self.lockfile.packages.iter().filter(|pkg| &pkg.name == pkg_name) {
+            
+        }
         // let version_req = match vulnerability.versions.patched().get(0) {
         //     Some(req) => req,
         //     None => fail!(ErrorKind::Version, "no fixed version available"),
