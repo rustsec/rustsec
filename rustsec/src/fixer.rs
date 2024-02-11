a0d8adf236bcdb551b7b9a2dfc3473003ac83db4
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-02-11 19:43:11 +0000

    Implement initial prototype of `cargo update`-based package upgrading

--- rustsec/src/fixer.rs
+++ rustsec/src/fixer.rs
@@ -1,11 +1,9 @@
 //! Automatically attempt to fix vulnerable dependencies
 
-use crate::{
-    error::{Error, ErrorKind},
-    vulnerability::Vulnerability,
-};
-use cargo_lock::Lockfile;
-use std::path::{Path, PathBuf};
+use crate::vulnerability::Vulnerability;
+use cargo_lock::{Lockfile, Package};
+use std::process::Command;
+use std::{path::Path, process::Output};
 
 /// Auto-fixer for vulnerable dependencies
 #[cfg_attr(docsrs, doc(cfg(feature = "fix")))]
@@ -23,27 +21,52 @@ impl<'a> Fixer<'a> {
         }
     }
 
-    /// Attempt to fix the given vulnerability
-    pub fn fix(&self, vulnerability: &Vulnerability, dry_run: bool) -> Result<(), Error> {
+    /// Attempt to fix the given vulnerability.
+    /// This function will succeed even if there is no semver-compatible fix available.
+    pub fn fix(&self, vulnerability: &Vulnerability, dry_run: bool) -> FixReport {
+        let mut outcomes = Vec::new();
         let cargo_path = std::env::var_os("CARGO").unwrap_or("cargo".into());
         let pkg_name = &vulnerability.package.name;
         // there can be more than one version of a given package in the lockfile, so we need to iterate over all of them
         // TODO: only consider vulnerable versions
-        for pkg in self.lockfile.packages.iter().filter(|pkg| &pkg.name == pkg_name) {
-            
+        for pkg in self
+            .lockfile
+            .packages
+            .iter()
+            .filter(|pkg| &pkg.name == pkg_name)
+        {
+            let mut command = Command::new(&cargo_path);
+            command.arg("--manifest-path").arg(self.manifest_path);
+            if dry_run {
+                command.arg("--dry-run");
+            }
+            let pkgid = pkgid(pkg);
+            command.arg(&pkgid);
+            // Sadly `cargo update` has no JSON output so we cannot reliably know the outcome
+            let output = command.output();
+            outcomes.push(FixOutcome {
+                package: pkg.to_owned(),
+                output: output,
+            })
         }
-        // let version_req = match vulnerability.versions.patched().get(0) {
-        //     Some(req) => req,
-        //     None => fail!(ErrorKind::Version, "no fixed version available"),
-        // };
 
-        // let dependency = cargo_edit::Dependency::new(vulnerability.package.name.as_str())
-        //     .set_version(&version_req.to_string());
-
-        // self.manifest.upgrade(&dependency, dry_run, false)?;
-
-        Ok(())
+        FixReport { outcomes }
     }
+}
+
+#[non_exhaustive]
+#[derive(Debug)]
+pub struct FixReport {
+    pub outcomes: Vec<FixOutcome>,
+}
+
+#[non_exhaustive]
+#[derive(Debug)]
+pub struct FixOutcome {
+    pub package: Package,
+    /// Output of the `cargo update` command,
+    /// including the stdout, stderr and exit code.
+    pub output: Result<Output, std::io::Error>,
 }
 
 /// Returns a Cargo unique identifier for a package.
@@ -51,7 +74,7 @@ impl<'a> Fixer<'a> {
 ///
 /// We need to pass these to `cargo update` because otherwise
 /// the package specification will be ambiguous, and it will refuse to do anything.
-fn pkgid(pkg: &cargo_lock::Package) -> String {
+fn pkgid(pkg: &Package) -> String {
     match pkg.source.as_ref() {
         Some(source) => format!("{}#{}@{}", source, pkg.name, pkg.version),
         None => format!("{}@{}", pkg.name, pkg.version),
