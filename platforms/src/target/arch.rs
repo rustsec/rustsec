72a3dcf82bf802f226ea777aefa58a78c6e33808
Ariel Ben-Yehuda <ariel.byd@gmail.com>
2025-05-21 22:38:03 +0300

    regenerate platform support for 1.87 and bump to platforms@3.6.0 (#1366)
    * regenerate platform support for 1.87 and bump to platforms@3.6.0
    
    quite a few platforms were removed so doing a minor bump
    
    * fix: allow platforms with an underscore followed by a letter in the name
    
    * fix: remove I586_PC_WINDOWS_MSVC from tested platforms since it was removed
    
    ---------
    
    Co-authored-by: Ariel Ben-Yehuda <arielby@amazon.com>

--- platforms/src/target/arch.rs
+++ platforms/src/target/arch.rs
@@ -13,6 +13,9 @@ pub enum Arch {
     /// `aarch64`: ARMv8 64-bit architecture
     AArch64,
 
+    /// `amdgpu`
+    Amdgpu,
+
     /// `arm`: 32-bit ARM architecture
     Arm,
 
@@ -97,6 +100,7 @@ impl Arch {
     pub fn as_str(self) -> &'static str {
         match self {
             Arch::AArch64 => "aarch64",
+            Arch::Amdgpu => "amdgpu",
             Arch::Arm => "arm",
             Arch::Arm64ec => "arm64ec",
             Arch::Avr => "avr",
@@ -134,6 +138,7 @@ impl FromStr for Arch {
     fn from_str(name: &str) -> Result<Self, Self::Err> {
         let result = match name {
             "aarch64" => Arch::AArch64,
+            "amdgpu" => Arch::Amdgpu,
             "arm" => Arch::Arm,
             "arm64ec" => Arch::Arm64ec,
             "avr" => Arch::Avr,
