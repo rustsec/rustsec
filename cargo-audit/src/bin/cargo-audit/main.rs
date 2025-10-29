38708d6fde4e8dcee60eca0292751029a87ae313
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-10-29 14:42:31 +0100

    cargo-audit: rely on built-in error display

--- cargo-audit/src/bin/cargo-audit/main.rs
+++ cargo-audit/src/bin/cargo-audit/main.rs
@@ -4,8 +4,8 @@
 #![forbid(unsafe_code)]
 
 use abscissa_core::{
-    Application, Component, Configurable, Runnable, Shutdown, application::fatal_error,
-    config::Override, terminal::ColorChoice, terminal::component::Terminal,
+    Application, Component, Configurable, Runnable, Shutdown, config::Override,
+    terminal::ColorChoice, terminal::component::Terminal,
 };
 use cargo_audit::{
     application::{APP, CargoAuditApplication},
@@ -16,7 +16,7 @@ use clap::Parser;
 use tracing_log::LogTracer;
 use tracing_subscriber::FmtSubscriber;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Parse command line options
     let command = CargoAuditCommand::parse();
 
@@ -25,9 +25,7 @@ fn main() {
     let terminal = Terminal::new(command.term_colors());
     let components = vec![Box::new(terminal) as Box<dyn Component<CargoAuditApplication>>];
 
-    if let Err(error) = LogTracer::init() {
-        fatal_error(&app, &error);
-    }
+    LogTracer::init()?;
 
     // Construct a tracing subscriber with the supplied filter and enable reloading.
     let subscriber = FmtSubscriber::builder()
@@ -44,45 +42,29 @@ fn main() {
         .finish();
 
     // Now set it as the global tracing subscriber and save the handle.
-    if let Err(error) = tracing::subscriber::set_global_default(subscriber) {
-        fatal_error(&app, &error)
-    }
-
-    if let Err(error) = app.state.components_mut().register(components) {
-        fatal_error(&app, &error);
-    };
+    tracing::subscriber::set_global_default(subscriber)?;
+    app.state.components_mut().register(components)?;
 
     // Load configuration
     let config = match command.config_path() {
-        Some(path) => match app.load_config(&path) {
-            Ok(cfg) => cfg,
-            Err(e) => fatal_error(&app, &e),
-        },
+        Some(path) => app.load_config(&path)?,
         None => AuditConfig::default(),
     };
 
     // Fire callback regardless of whether any config was loaded to
     // in order to signal state in the application lifecycle
     let config = match &command.cmd {
-        CargoAuditSubCommand::Audit(cmd) => match cmd.override_config(config) {
-            Ok(cfg) => cfg,
-            Err(e) => fatal_error(&app, &e),
-        },
+        CargoAuditSubCommand::Audit(cmd) => cmd.override_config(config)?,
     };
 
-    if let Err(error) = app.state.components_mut().after_config(&config) {
-        fatal_error(&app, &error);
-    }
-
     // Run the command
+    app.state.components_mut().after_config(&config)?;
     app.config.set_once(config);
     let app = APP.get_or_init(|| app);
     command.run();
 
     // Exit gracefully
     let components = app.state().components();
-
-    if let Err(e) = components.shutdown(app, Shutdown::Graceful) {
-        fatal_error(app, &e)
-    }
+    components.shutdown(app, Shutdown::Graceful)?;
+    Ok(())
 }
