9316b7f0b70e45e2aeb23b1d603ece73bf2da8ed
Christian Banse <oxisto@aybaze.com>
2025-11-24 13:58:17 +0100

    Added temporal score equation

--- cvss/src/v3/temporal.rs
+++ cvss/src/v3/temporal.rs
@@ -5,3 +5,24 @@ mod rc;
 mod rl;
 
 pub use self::{e::ExploitCodeMaturity, rc::ReportConfidence, rl::RemediationLevel};
+
+use crate::v3::Metric;
+use crate::v3::{Score, Vector};
+
+impl Vector {
+    /// Calculates Temporal CVSS score.
+    ///
+    /// Described in CVSS v3.1 Specification: Section 7.2:
+    /// <https://www.first.org/cvss/v3-1/specification-document#7-2-Temporal-Metrics-Equations>
+    fn temporal_score(&self) -> Score {
+        let base_score = self.base_score().value();
+
+        let e_score = self.e.map(|e| e.score()).unwrap_or(0.0);
+        let rl_score = self.rl.map(|rl| rl.score()).unwrap_or(0.0);
+        let rc_score = self.rc.map(|rc| rc.score()).unwrap_or(0.0);
+
+        let temporal_score = base_score * e_score * rl_score * rc_score;
+
+        Score::new(temporal_score).roundup()
+    }
+}
