a64beb3ffc76c50f56322015e09963759e3f3d91
42triangles <42triangles@tutanota.com>
2026-01-15 16:50:55 +0100

    Change `doc_auto_cfg` feature usage back to `doc_cfg`
    The [most recent docs.rs build failed](https://docs.rs/crate/cargo-lock/latest/builds/2643451) because this feature was since merged into `doc_cfg`.

--- cargo-lock/src/lib.rs
+++ cargo-lock/src/lib.rs
@@ -1,6 +1,6 @@
 #![doc = include_str!("../README.md")]
 #![doc(html_logo_url = "https://raw.githubusercontent.com/RustSec/logos/main/rustsec-logo-lg.png")]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
 
