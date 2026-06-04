44833f63cf39093df52fa28edc4f11caaa5602c1
Dirkjan Ochtman <dirkjan@ochtman.nl>
2026-06-04 15:14:40 +0200

    admin: improve error handling in assigner

--- admin/src/assigner.rs
+++ admin/src/assigner.rs
@@ -1,16 +1,18 @@
 //! RustSec Advisory DB tool to assign ids
 
-use crate::{Map, error::ErrorKind, prelude::*};
-use rustsec::{
-    Advisory, Collection,
-    advisory::{IdKind, Parts},
-};
 use std::{
     fs::{self, File},
     io::{BufRead, BufReader, LineWriter, Write},
     path::Path,
     process::exit,
 };
+
+use rustsec::{
+    Advisory, Collection,
+    advisory::{IdKind, Parts},
+};
+
+use crate::{Map, prelude::*};
 
 /// What sort of output should be generated on stdout.
 #[derive(PartialEq, Eq, Clone, Copy)]
@@ -78,7 +80,14 @@ pub fn assign_ids(repo_path: &Path, output_mode: OutputMode) {
             &mut highest_id,
             output_mode,
             &mut assignments,
-        );
+        )
+        .unwrap_or_else(|error| {
+            status_err!(
+                "Error assigning ids for {collection} in {}: {error}",
+                repo_path.display(),
+            );
+            exit(1);
+        });
     }
 
     if output_mode != OutputMode::GithubAction {
@@ -103,37 +112,53 @@ fn assign_ids_across_directory(
     highest_ids: &mut Map<u32, u32>,
     output_mode: OutputMode,
     assignments: &mut Vec<String>,
-) {
+) -> Result<(), rustsec::Error> {
     let dir_path = repo_path.join(collection.to_string());
     let Ok(collection_entry) = fs::read_dir(dir_path) else {
-        return;
+        return Ok(());
     };
 
     for dir_entry in collection_entry {
-        let unwrapped_dir_entry = dir_entry.unwrap();
-        let dir_name = unwrapped_dir_entry.file_name().into_string().unwrap();
+        let unwrapped_dir_entry = dir_entry?;
+        let dir_name = unwrapped_dir_entry
+            .file_name()
+            .into_string()
+            .map_err(|os_str| {
+                rustsec::Error::new(
+                    rustsec::ErrorKind::Parse,
+                    format!("Couldn't parse directory name: {}", os_str.display()),
+                )
+            })?;
+
         let dir_path = unwrapped_dir_entry.path();
         let dir_path_clone = dir_path.clone();
-        for advisory_entry in fs::read_dir(dir_path).unwrap() {
-            let unwrapped_advisory = advisory_entry.unwrap();
+        for advisory_entry in fs::read_dir(dir_path)? {
+            let unwrapped_advisory = advisory_entry?;
             let advisory_path = unwrapped_advisory.path();
             if !Advisory::is_draft(&advisory_path) {
                 continue;
             }
 
-            let advisory_data = fs::read_to_string(&advisory_path)
-                .map_err(|e| {
-                    format_err!(
-                        ErrorKind::Io,
-                        "Couldn't open {}: {}",
-                        advisory_path.display(),
-                        e
-                    );
-                })
-                .unwrap();
+            let advisory_data = fs::read_to_string(&advisory_path).map_err(|e| {
+                rustsec::Error::with_source(
+                    rustsec::ErrorKind::Io,
+                    format!("Couldn't open {}", advisory_path.display()),
+                    e,
+                )
+            })?;
 
-            let advisory_parts = Parts::parse(&advisory_data).unwrap();
-            let advisory: Advisory = toml::from_str(advisory_parts.front_matter).unwrap();
+            let advisory_parts = Parts::parse(&advisory_data)?;
+            let advisory: Advisory = toml::from_str(advisory_parts.front_matter).map_err(|e| {
+                rustsec::Error::with_source(
+                    rustsec::ErrorKind::Parse,
+                    format!(
+                        "Couldn't parse TOML front matter in {}",
+                        advisory_path.display()
+                    ),
+                    e,
+                )
+            })?;
+
             let date = advisory.metadata.date;
             let year = date.year();
             let new_id = highest_ids.get(&year).cloned().unwrap_or_default() + 1;
@@ -141,25 +166,21 @@ fn assign_ids_across_directory(
             let string_id = format!("RUSTSEC-{year_str}-{new_id:04}");
             let new_filename = format!("{string_id}.md");
             let new_path = dir_path_clone.join(new_filename);
-            let original_file = File::open(&advisory_path).unwrap();
+            let original_file = File::open(&advisory_path)?;
             let reader = BufReader::new(original_file);
-            let new_file = File::create(new_path).unwrap();
+            let new_file = File::create(new_path)?;
             let mut writer = LineWriter::new(new_file);
             for line in reader.lines() {
-                let current_line = line.unwrap();
+                let current_line = line?;
                 if current_line.trim() == "id = \"RUSTSEC-0000-0000\"" {
-                    writer
-                        .write_all(format!("id = \"{string_id}\"\n").as_ref())
-                        .unwrap();
+                    writer.write_all(format!("id = \"{string_id}\"\n").as_ref())?;
                 } else {
                     let current_line_with_newline = format!("{current_line}\n");
-                    writer
-                        .write_all(current_line_with_newline.as_ref())
-                        .unwrap();
+                    writer.write_all(current_line_with_newline.as_ref())?;
                 }
             }
             highest_ids.insert(year, new_id);
-            fs::remove_file(&advisory_path).unwrap();
+            fs::remove_file(&advisory_path)?;
             if output_mode == OutputMode::HumanReadable {
                 status_ok!("Assignment", "Assigned {} to {}", string_id, dir_name);
             } else {
@@ -167,4 +188,6 @@ fn assign_ids_across_directory(
             }
         }
     }
+
+    Ok(())
 }
